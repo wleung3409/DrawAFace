4e695a7f6fe8b321f71acac13f2f4ae66c4771d2
William <wleung3409@bths.edu>
2018-12-02 21:51:03 -0500

    done!

--- src/com/company/DrawAFaceRunner.java
+++ src/com/company/DrawAFaceRunner.java
@@ -1,12 +1,37 @@
 package com.company;
 
-public class DrawAFaceRunner {
+    public class DrawAFaceRunner{
+        public static void main(String[]args){
+            String[][]template = new String[][]{
+                    {" ","0","0","0","0","0","0","0","0","0"," "},
+                    {" ","0","0","0","0","0","0","0","0","0"," "},
+                    {" ","0","0","0","0","0","0","0","0","0"," "},
+                    {" ","0","0","0","0","0","0","0","0","0"," "},
+                    {" ","0","0","0","0","0","0","0","0","0"," "},
+                    {" ","0","0","0","0","0","0","0","0","0"," "},
 
-    public static void main(String[] args) {
-	// write your code here
-        String[][] face = new String [5][8];
-        String FaceDrawing = new FaceDrawing(face);
-        String face1 ;
+
+            };
+            FaceDrawing Face = new FaceDrawing(template);
+
+            System.out.println(Face.toString());
+
+            Face.fill("0");
+
+            System.out.println(Face.toString());
+
+            Face.edit("^",1,2);
+            Face.edit("^",1,8);
+            Face.edit("~",3,2);
+            Face.edit("~",3,3);
+            Face.edit("~",3,4);
+            Face.edit("~",3,5);
+            Face.edit("~",3,6);
+            Face.edit("~",3,7);
+            Face.edit("~",3,8);
+            Face.edit("~",3,8);
+
+            System.out.println(Face.toString());
 
     }
 }
