48e897df43303778d3a1261babf224f115ea383a
William <wleung3409@bths.edu>
2018-12-02 20:34:30 -0500

    almost

--- src/com/company/FaceDrawing.java
+++ src/com/company/FaceDrawing.java
@@ -1,9 +1,20 @@
 package com.company;
 
 public class FaceDrawing {
-    extends java.lang.Object
-    FaceDrawing(java.lang.String[][] faceFeatures){
-        toString();
+    private String[][] face;
+    public FaceDrawing(String[][][]face) {
+        this.face = face;
+    }
+
+    public void edit(String str,int row,int col) {
+        this.face[row][col] = str;
+    }
+    public void fill (String str){
+        for(int i=0;i<this.face.length;i++){
+            for(int n=0;n<this.face[i].length;n++){
+                this.face[i][n] = str;
+            }
+        }
 
 }
 }
